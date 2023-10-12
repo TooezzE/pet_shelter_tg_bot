0460021f10cfc359945c6ae1b5f6903dee4796fd
Dinar <123669430+Dinfat696@users.noreply.github.com>
2023-10-12 23:24:11 +0300

    keyboard

--- src/main/java/pet/shelter/commands/CommandName.java
+++ src/main/java/pet/shelter/commands/CommandName.java
@@ -10,7 +10,22 @@ public enum CommandName {
     SAFETY_TIPS("/safetytips"),
     GUESS_INFO("/contactinfo"),
     VOLUNTEER("/volunteer"),
-    UNKNOWN("");
+    UNKNOWN(""),
+    CAT("/cat"),
+    DOG("/dog");
+
+//  START("/start"),
+//    CAT("Кошки"),
+//    DOG("Собаки"),
+//    HOW_ADOPT_PET_INFO("Как взять животного из приюта"),
+//    VOLUNTEER("Позвать волонтера"),
+//    SHELTER_INFO("Информация о приюте"),
+//    SHELTER_ADDRESS_SCHEDULE("Адрес и график работы приюта"),
+//    RECOMMENDATIONS_LIST("Список рекомендаций и советов"),
+//    DOCUMENTS_LIST("Список необходимых документов"),
+//    SEND_CONTACT("Отправить контактные данные"),
+//    SEND_REPORT("Прислать отчет о питомце"),
+//    UNKNOWN("");
 
     private final String commandName;
 
---
+++ src/main/java/pet/shelter/keyboard/KeyBoard.java
@@ -0,0 +1,44 @@
+package pet.shelter.keyboard;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pet.shelter.commands.CommandName;
+
+public class KeyBoard {
+    private final Logger logger= LoggerFactory.getLogger(KeyBoard.class);
+    private TelegramBot telegramBot;
+
+    public KeyBoard(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    public void choiceMenu(Long chatId) {//выбираем приют
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(
+                CommandName.CAT.getCommandName(), CommandName.DOG.getCommandName());
+        sendResponseMenu(chatId, replyKeyboardMarkup, "Выберите приют");
+    }
+    public void shelterMainMenu(long chatId) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(
+                new String[]{CommandName.INFO.getCommandName(), CommandName.КАК ВЗЯТЬ ЖИВОТНОЕ.getCommandName()},
+                new String[]{CommandName.ОТЧЕТ.getCommand(), CommandName.VOLUNTEER.getCommandName()});
+        sendResponseMenu(chatId, replyKeyboardMarkup, "Ниже представлено главное меню приюта. " +
+                "Чтобы вернуться к выбору приюта, напишите команду /start");
+    }
+
+
+
+
+
+
+    public void sendResponseMenu(long chatId, ReplyKeyboardMarkup replyKeyboardMarkup, String message) {
+        SendMessage sendMessage = new SendMessage(
+                chatId, message).replyMarkup(replyKeyboardMarkup.resizeKeyboard(true));
+        SendResponse sendResponse = telegramBot.execute(sendMessage);
+        if (!sendResponse.isOk()) {
+            logger.error("Error during sending message: {}", sendResponse.description());
+        }
+    }
